c40fc0a675c4cf653eaa1e9aef817bd55ebee451
oliyg <billyangg@qq.com>
2023-05-12 16:54:11 +0800

    Add Icon toggle

--- dictionary/src/components/DarkMode.vue
+++ dictionary/src/components/DarkMode.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import IconToggleDisableVue from './icons/IconToggleDisable.vue';
+import IconToggleEnableVue from './icons/IconToggleEnable.vue';
 import IconHalfMoon from './icons/IconHalfMoon.vue';
+import { ref } from 'vue';
+
+const bool = ref(false)
+const toggle = () => (bool.value = !bool.value)
 </script>
 
 <template>
-    <IconToggleDisableVue class="mr-3 w-10 h-5 tablet:mr-5" />
+    <IconToggleDisableVue @click="toggle" v-if="!bool" class="mr-3 w-10 h-5 tablet:mr-5 cursor-pointer" />
+    <IconToggleEnableVue v-else @click="toggle" class="mr-3 w-10 h-5 tablet:mr-5 rotate-180 cursor-pointer" />
     <IconHalfMoon class="w-5 h-5" />
 </template>
---
+++ dictionary/src/components/icons/IconToggleEnable.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg width="40" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="40" height="20" rx="10" fill="#A445ED" />
+    <circle cx="10" cy="10" r="7" fill="#fff" />
+  </svg>
+</template>
