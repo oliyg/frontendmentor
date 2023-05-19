ffba98bb82f57cb589255521a5273ad12cd4c28d
oliyg <billyangg@qq.com>
2023-05-19 16:38:44 +0800

    style change

--- multi-step-form/src/App.jsx
+++ multi-step-form/src/App.jsx
@@ -4,7 +4,7 @@ import Nav from "./Nav";
 
 function App() {
   return (
-    <div className="w-full desktop:rounded-[0.63rem] desktop:p-4 desktop:w-[58.75rem] desktop:flex desktop:justify-between bg-gray-100">
+    <div className="w-full desktop:rounded-[0.63rem] desktop:p-4 desktop:w-[58.75rem] desktop:flex desktop:justify-between desktop:bg-gray-100">
       <Nav />
       <Content />
     </div>
